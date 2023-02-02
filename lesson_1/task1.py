f0fcfe0975660d48be954164161a4bc6bfba0fa6
aslanyan66 <aslanyanazat66@gmail.com>
2023-02-02 20:04:55 +0400

    lesson_1

---
+++ lesson_1/task1.py
@@ -0,0 +1,27 @@
+# task 2
+print("Azat Aslanyan")
+
+# task 3
+current_year = 2023
+my_birth_year = 1996
+print(current_year - my_birth_year)
+
+# task 4
+full_name = "Azat Aslanyan"
+age = 26
+print(full_name, age)
+
+# task 5
+passed_lesson = "We have passed number, float, boolean and string data types.Beside SnakeCase we also can define variables Camelcase and PascalCase ways."
+print(passed_lesson)
+
+# task 6
+
+# The part of comment I got.
+
+# task 7
+x = 16
+y = 5
+z = x // y == 3
+
+print(type(z))
