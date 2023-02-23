e4d58fcd681f9159292c487dab7b828820438459
aslanyan66 <aslanyanazat66@gmail.com>
2023-02-23 20:34:22 +0400

    fixes

--- lesson_11/homework11.py
+++ lesson_11/homework11.py
@@ -1,21 +1,15 @@
 # *** Task 1
 
-def get_users_data(file_path):
-  with open(file_path) as file:
-    data = file.readlines()[1:]
-  return [values.strip().split(',') for values in data]
-
-
-def get_keys(file_path):
+# *** 1.1 start
+def get_keys(file_path='db.txt'):
   with open(file_path) as file:
     keys = file.readlines()[0].strip()
   return keys.split(',')
 
-
-def add_data_to_db(new_data, db_path='db.txt'):
-  with open(db_path, 'a') as db:
-    db.write(f'\n{new_data}')
-
+def get_users_data(file_path='db.txt'):
+  with open(file_path) as file:
+    data = file.readlines()[1:]
+  return [values.strip().split(',') for values in data]
 
 def create_users(keys, data):
   users = []
@@ -24,26 +18,44 @@ def create_users(keys, data):
     users.append(dict(zip(keys, values)))
   return users
 
+users = create_users(get_keys(), get_users_data())
 
-def add_user(users, **kwargs):
+print(users, '1.1')
+
+# *** 1.1 end
+
+
+
+# ### 1.2 start
+
+def add_user_to_current_list(users, **kwargs):
   users.append(kwargs)
-  print(kwargs.values(), 'a')
-  add_data_to_db(','.join(kwargs.values()))
-
 
 my_data = {
   'first_name': 'Azat', 'last_name': 'Aslanyan', 'age': '26', 'profession': 'quick learner', 'country': 'Armenia',
   'favorite_film': 'Gattaca', 'favorite_singer': 'chgidem', 'favorite_chips': 'lays'
 }
 
-users = create_users(get_keys('db.txt'), get_users_data('db.txt'))
-add_user(users, **my_data)
+add_user_to_current_list(users, **my_data)
+
+print(users, 'task 1.2')
+# ### 1.2 end
+
+
+
+# ### 1.3 start
+def add_data_to_db(new_data, db_path='db.txt'):
+  with open(db_path, 'a') as db:
+    db.write(f'\n{new_data}')
+
+add_data_to_db(','.join(users[-1].values()))
+# ### 1.3 end
 
 # *** Task 2
 
 get_Number = lambda val: val[0] if not isinstance(val[0], list) else get_Number(val[0])
 
-print(get_Number([[[[[10]]]]]))
+print(get_Number([[[[[10]]]]]), 'task 2')
 
 
 # *** Task 3
@@ -58,7 +70,7 @@ def get_sum(numbers_list, total=0, idx=0):
   return get_sum(numbers_list, total, idx + 1)
 
 
-print(get_sum([1,2]))
+print(get_sum([1, 2]), 'task 3')
 
 
 # Research
