f5a303e350d3e3a40ebfdb9cd8cc892a7768d356
aslanyan66 <aslanyanazat66@gmail.com>
2023-03-12 14:14:22 +0400

    done homework 15

---
+++ lesson_15/homework15.py
@@ -0,0 +1,33 @@
+# *** Task 1
+data = ['abcba', 'abcbca', 'abcbt', '121', '123454321', '5551555']
+symmetric_items = list(filter(lambda item: item == item[::-1], data))
+
+print(symmetric_items, 'task 1')
+
+# *** Task 2
+
+def custom_map(cb, sequence):
+  return (cb(item) for item in sequence)
+
+
+# *** Task 3
+
+numbers = [1, 2, 5, 70, 4, 8, 50, 44]
+
+# *** task 3.1
+def get_max_num(numbers):
+  max_num = float('-inf')
+  for num in numbers:
+    max_num = num if max_num < num else max_num
+  return max_num
+
+print(get_max_num(numbers), 'Task 3.1')
+
+
+# *** Task 3.2
+
+from functools import reduce
+
+max = reduce(lambda max, item: item if max < item else max, numbers, float('-inf'))
+
+print(max, 'Task 3.2')
